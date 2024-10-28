fa6fe8e3fcc6b1acbe20524808a6b69607cfa25f
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2024-10-28 12:36:07 +0000

    Add main.py

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.database import Session
+
+
+def main():
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+    # Solicitando dados para o usuário.
+    print("\nAdicionado Usuário.")
+    nome = input("Digite seu nome:")
+    email = input("Digite seu e-mail:")
+    senha = input("Digite sua senha:")
+
+
+    service.criar_usuario(nome= nome, email= email, senha= senha)
+
+    
+    # Listar todos os usuários cadastrados
+    print("\nListar usuários cadastrados")
+    listar_usuarios = service.listar_todos_usuarios()
+    for usuario in listar_usuarios:
+        print(f"Nome: {usuario.nome} - Email: {usuario.email} - Senha: {usuario.senha}")
+
+
+
+if __name__ == "__main__":
+    main()
--- app/models/usuario.py
+++ app/models/usuario.py
@@ -5,6 +5,7 @@ from config.database import db
 Base = declarative_base()
 
 class Usuario(Base):
+    #Definindo características da tabela no banco de dados 
     __tablename__ = "usuarios"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -13,7 +14,7 @@ class Usuario(Base):
     senha = Column(String(150))
 
 
-    def __init__(self, nome, email, senha):
+    def __init__(self, nome: str, email: str, senha: str):
         self.nome = nome
         self.email = email
         self.senha = senha
