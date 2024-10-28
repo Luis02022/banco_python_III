828f0518e8d87cb61770a2f9ba9a9da34e8a05d6
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2024-10-28 11:41:09 +0000

    Add databse.py

---
+++ app/config/database.py
@@ -0,0 +1,33 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+
+# Parâmetros para a conexão com BD MySQL
+db_user = "user"
+db_password = "user_password"
+db_host = "localhost"
+db_port = "3306"
+db_name = "meu_banco"
+
+# Endereço/caminho para a conexão com BD MySQL
+DATABASE_URL = f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+
+#Conectando ao banco de dados 
+db = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=db)
+session = Session()
+
+#Gerenciando  sessão 
+@contextmanager
+def get_db():
+    db = Session()
+    try:
+        yield db
+        db.commit()# Se der certo, faz commit
+    except Exception as erro:
+        db.rollback()
+        raise erro 
+    finally:
+        db.close()
